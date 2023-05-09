fecc29d9d6e05dc9f14644044eb97009b8fb3b64
suhail23599 <suhail23599@gmail.com>
2023-05-10 00:36:27 +0700

    Refs and reactive functions

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  
+  <div>
+    App.vue
+    {{ abc }}
+    {{ utilVarRef }}
+    {{ utilVarReact.key }}
+  </div>
+  <test-1 />
 </template>
 
 <script>
-
+import { ref, reactive } from 'vue'
+import { utilVarRef, utilVarReact } from './Utils/util'
+import Test1 from './components/Test1.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Test1
+  },
+  setup () {
+    let abc = ref('testing')
+    let userDetails = reactive({
+      name: 'Jhon',
+      age: '25'
+    })
+    abc.value = 2
+    utilVarRef.value = 'changed'
+    utilVarReact.key = 'react-changed' // no need to mention .value to access key.
+    return {
+      abc,
+      utilVarRef,
+      userDetails,
+      utilVarReact
+    }
   }
 }
 </script>
