b8862b279057d6f7a78cac2250b1cd1f89d2eb77
suhail23599 <suhail23599@gmail.com>
2023-05-10 13:02:28 +0700

    more on reactive

---
+++ src/Utils/util.js
@@ -0,0 +1,5 @@
+import { reactive, ref } from 'vue'
+export const utilVarRef = ref('test-1')
+export const utilVarReact = reactive({
+   key: 'value'
+})
---
+++ src/components/Test1.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+      Test 1 {{utilVarRef}}
+   </div>
+</template>
+
+<script>
+import { utilVarRef } from '../Utils/util.js'
+export default {
+   setup() {
+      console.log(utilVarRef.value)
+      return {
+         utilVarRef
+      }
+   }
+}
+</script>
