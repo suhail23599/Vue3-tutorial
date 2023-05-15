ed4d702bd12533774a399d62cf341744a013a0a9
suhail23599 <suhail23599@gmail.com>
2023-05-15 18:33:44 +0700

    router

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      This is about page
+   </div>
+</template>
+
+<script>
+
+export default {
+   name: 'About',
+   setup() {
+      console.log('setup')
+   }
+}
+</script>
